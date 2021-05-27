f48c576edc88a119eeb421b73dc252e7aff32406
Henrique Moura de Mesquita <moura.henrique@outlook.com>
2021-05-27 01:47:02 -0300

    Add files via upload

---
+++ ex028.py
@@ -0,0 +1,17 @@
+import random
+print('O computador está pensando em um número.')
+print('... pensando...')
+print('... consegue adivinhar?')
+r = 1
+a = 2
+n = 3
+d = 4
+o = 5
+lista = [r, a, n, d, o]
+num = int(input('Escolha um número de 1 a 5: '))
+sorteio = random.choice(lista)
+if num == sorteio:
+    print('Você acertou, o número é {}'.format(sorteio))
+else:
+    print('Você errou, tente novamente.')
+
